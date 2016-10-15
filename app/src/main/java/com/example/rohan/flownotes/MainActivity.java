97b7ea2a05d9005b627067b02bb8063172c9e4bd
rohanperi <perirohan@gmail.com>
2016-10-15 10:04:57 -0400

    Comment change

--- app/src/main/java/com/example/rohan/flownotes/MainActivity.java
+++ app/src/main/java/com/example/rohan/flownotes/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // This is testing my branch
+        // This is testing my branch 1
     }
 }
