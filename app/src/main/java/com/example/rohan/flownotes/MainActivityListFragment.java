272a0f415983a9c89a5c60b795a45c4922b0e40e
rohanperi <perirohan@gmail.com>
2016-10-16 07:03:45 -0400

    new files added

--- app/src/main/java/com/example/rohan/flownotes/MainActivityListFragment.java
+++ app/src/main/java/com/example/rohan/flownotes/MainActivityListFragment.java
@@ -2,6 +2,7 @@ package com.example.rohan.flownotes;
 
 
 import android.app.ListFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
@@ -51,6 +52,19 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onListItemClick(ListView  l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+
+        launchNoteDetailActivity(position);
+    }
+
+    private void launchNoteDetailActivity(int position) {
+
+        Note note = (Note) getListAdapter().getItem(position);
+        Intent intent = new Intent(getActivity(), NoteDetailActivity.class);
+        intent.putExtra(NoteActivity.NOTE_MESSAGE_EXTRA, note.getMessage());
+        intent.putExtra(NoteActivity.NOTE_CATEGORY_EXTRA, note.getCategory());
+        intent.putExtra(NoteActivity.NOTE_ID_EXTRA, note.getId());
+
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/rohan/flownotes/NoteActivity.java
+++ app/src/main/java/com/example/rohan/flownotes/NoteActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class NoteActivity extends AppCompatActivity {
 
+    public static final String NOTE_ID_EXTRA = "com.example.rohan.flownotes.Identifier";
+    public static final String NOTE_MESSAGE_EXTRA = "com.example.rohan.flownotes.Message";
+    public static final String NOTE_CATEGORY_EXTRA = "com.example.rohan.flownotes.Category";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
