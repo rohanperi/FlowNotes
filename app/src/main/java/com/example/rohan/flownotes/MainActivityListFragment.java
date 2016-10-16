721aed767022704955871d68234feac7ba0f3c46
rohanperi <perirohan@gmail.com>
2016-10-15 21:31:29 -0400

    logos backup

--- app/src/main/java/com/example/rohan/flownotes/MainActivityListFragment.java
+++ app/src/main/java/com/example/rohan/flownotes/MainActivityListFragment.java
@@ -5,6 +5,7 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +15,13 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        String[] values = new String[]{"Android", "IPhone", "Windows", "Blackberry", "Ubuntu", "Lord Gaben"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+
+        setListAdapter(adapter);
 
     }
 
